6d30b92bd3ecc5e8fc603bce5340f1379e3ff607
Repsej <repsejlysras@gmail.com>
2016-08-20 16:15:46 +0200

    Added saving and trying the last used IP without USB access

--- src/dk/appdictive/Main.java
+++ src/dk/appdictive/Main.java
@@ -1,16 +1,75 @@
 package dk.appdictive;
 
 import java.io.*;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main
 {
-    private static final String IP_ADDRESS_REGEX = "(?:[0-9]{1,3}\\.){3}[0-9]{1,3}";
+    private static final String IP_ADDRESS_REGEX = "(?:[0-9]{1,3}\\.){3}[0-9]{1,3}", PREF_IP_NAME = "SAVED_IP";
 
     public static void main(String args[])
     {
-        String output = runCommand("adb -d shell ip -f inet addr show wlan0");
+        String ipAddress = "";
+
+        // Retrieve the user preference node for the package
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+
+        // Get the value of the preference;
+        // default value is returned if the preference does not exist
+        String defaultValue = "";
+        String propertyValue = prefs.get(PREF_IP_NAME, defaultValue); // "a string"
+
+        System.out.println("Trying saved IP");
+
+        if (propertyValue.equals("")) {
+            //if no IP has been previously saved, get the current
+            ipAddress = getIP();
+        } else {
+            ipAddress = propertyValue;
+        }
+
+        System.out.println("IP: " + ipAddress);
+
+        if (connectToIP(ipAddress)) {
+            System.out.println("Connected: " + ipAddress);
+            saveIP(ipAddress, prefs);
+        } else {
+            ipAddress = getIP();
+            if (connectToIP(ipAddress)) {
+                System.out.println("Connected: " + ipAddress);
+                saveIP(ipAddress, prefs);
+            }
+        }
+
+        System.out.println("Done");
+    }
+
+    public static void saveIP(String ip, Preferences prefs) {
+        prefs.put(PREF_IP_NAME, ip);
+        try {
+            prefs.flush();
+        } catch (BackingStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean connectToIP(String ip) {
+        String port = "5555";
+        runCommand("adb tcpip " + port);
+        String output = runCommand("adb connect " + ip + ":" + port);
+        System.out.println(output);
+        if (output.contains("unable")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static String getIP() {
+        String output = runCommand("adb shell ip -f inet addr show wlan0");
         System.out.println(output);
 
         Pattern ipAddressPattern = Pattern.compile(IP_ADDRESS_REGEX);
@@ -25,14 +84,9 @@ public class Main
             }
         }
 
-        System.out.println("IP: " + ipAddress);
+//        System.out.println(ipAddress);
 
-        String port = "5555";
-        runCommand("adb tcpip " + port);
-        output = runCommand("adb connect " + ipAddress + ":" + port);
-        System.out.println(output);
-
-        System.out.println("Done");
+        return ipAddress;
     }
 
     public static String runCommand(String command) {
@@ -56,5 +110,9 @@ public class Main
 
         return null;
     }
+
+
+
+
 
 }
